ab42dfd8054d7adb5919c99aecf12b19550dad3e
xch <xch@qq.com>
2018-06-25 10:57:14 +0800

    在appModule中添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'heroes', pathMatch: 'full'},
+  {path: 'crisis-center', component: CrisisCenterComponent},
+  {path: 'heroes', component: HeroListComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisCenterComponent,
+    HeroListComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
